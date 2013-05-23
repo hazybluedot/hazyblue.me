aa952d510796272f626111b61735e456b60d33a0
Darren Maczka <dmaczka@vt.edu>
2013-05-22 20:54:08 -0400

    update

--- lib/helper.rb
+++ lib/helper.rb
@@ -1,2 +1,23 @@
 include Nanoc::Helpers::Tagging
+include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::LinkTo
+include Nanoc::Helpers::Rendering
+
+module PostHelper
+
+  def get_pretty_date(post)
+    attribute_to_time(post[:created_at]).strftime('%B %-d, %Y')
+  end
+
+  def get_post_start(post)
+    content = post.compiled_content
+    if content =~ /\s<!-- more -->\s/
+      content = content.partition('<!-- more -->').first +
+        "<div class='read-more'><a href='#{post.path}'>Continue reading &rsaquo;</a></div>"
+    end
+    return content
+  end
+
+end
+
+include PostHelper
