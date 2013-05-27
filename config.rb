ab497167a581203c08413752cb4c8248451d5a9a
Darren Maczka <dmaczka@vt.edu>
2013-05-26 22:58:30 -0400

    complete rename of stylz to stylesheets

--- config.rb
+++ config.rb
@@ -2,8 +2,8 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "output/stylz"
-sass_dir = "content/stylz"
+css_dir = "output/stylesheets"
+sass_dir = "content/stylesheets"
 images_dir = "output/images"
 javascripts_dir = "output/javascripts"
 
@@ -16,5 +16,5 @@ javascripts_dir = "output/javascripts"
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 
-preferred_syntax = :sass
+preferred_syntax = :scss
 
