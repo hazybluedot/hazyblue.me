206aa00c3ee8f4be8654699b58561e07330b765e
Darren Maczka <dmaczka@vt.edu>
2013-06-05 20:14:34 -0400

    add you tube url -> embedded video filter

--- lib/helper.rb
+++ lib/helper.rb
@@ -23,4 +23,27 @@ module PostHelper
   end
 end
 
+class YouTubeFilter < Nanoc::Filter
+  identifier :youtube
+  type :text
+  
+  def run(content, params={})
+#    content.gsub(/[a-z]+/, 'nanoc rules')
+    content.each_line.map do |line|
+      youtube_id = nil
+      if line[/^\s*https?:\/\/youtu\.be\/([^\?]*)/]
+        youtube_id = $1
+      elsif line[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+        youtube_id = $5
+      end
+      
+      if youtube_id
+        line  = %Q{<p><iframe title="YouTube video player" width="584" height="329" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe></p>}
+      else
+        line
+      end
+    end.join("")
+  end
+end
+
 include PostHelper
