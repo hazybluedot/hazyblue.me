c4c476b713ec43ef7a357d441466a88bacae6432
Darren Maczka <dmaczka@vt.edu>
2013-07-05 14:06:12 -0400

    crack open Nanoc::Item to add 'comments' method. Good practice? who knows?

--- lib/comments.rb
+++ lib/comments.rb
@@ -3,11 +3,23 @@ require 'nanoc/toolbox'
 include Nanoc::Toolbox::Helpers::Gravatar
 
 module Nanoc::Helpers
+  module LinkTo
+    def external_url base
+      if base =~ /^https?:\/\//
+        base
+      else
+        base = 'http://' + base
+      end
+    end
+  end
+end
+
+module Nanoc::Helpers
   module StaticComments
 
     def comment_author_link(comment)
-      if comment[:website]
-        link_to(comment[:author], comment[:website])
+      if comment[:website] and comment[:website].length > 0
+        link_to(comment[:author], external_url(comment[:website]))
       else
         comment[:author]
       end
@@ -19,12 +31,24 @@ module Nanoc::Helpers
       gravatar_image(email, :size => '50')
     end
 
-    # Find all the comments for a post
+    # Find and return all the comments for a post
+    # @param [Nanoc::Item] the nanoc item used to match comments to
+    # @return [Array<Nanoc::Item>] The comments for the supplied item, if any    
     def comments_for_post(post)
-      comments = @items.select { |item| item.identifier =~ /^\/comments\// and item[:post_id] == post.identifier }
+      comments = @site.items.select { |item| item.identifier =~ /^\/comments\// and item[:post_id] == post.identifier }
       comments.sort_by { |a| attribute_to_time(a[:created_at]) } 
     end
 
   end
 end
 
+include Nanoc::Helpers::StaticComments
+
+module Nanoc
+  class Item
+    # @return [Array<Nanoc::Item>] The comments for this item, if any
+    def comments
+      comments_for_post(self)
+    end
+  end
+end
