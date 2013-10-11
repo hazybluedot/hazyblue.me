5ce9800b80cb123cc85830f027b42219ad451582
Darren Maczka <dmaczka@vt.edu>
2013-10-11 16:43:00 -0400

    fix identifier bug
    nanoc Filesystem data source expects identifiers to have a trailing /
    and blindly strips the last character from identifier variable.

--- commands/create_post.rb
+++ commands/create_post.rb
@@ -1,5 +1,5 @@
 usage 'create_post [options] title'
-aliases :create_post, :cp
+aliases :create_post, :edit_post, :cp
 description 'Create a new blog post'
 
 option :c, :vcs, 'versioning control system', :argument => :required
@@ -18,14 +18,19 @@ class CreateItemComment < Nanoc::CLI::Commands::CreateItem
       :published => false,
       :kind => 'article',
       :created_at => Time.now.strftime(datetime_format),
-      :author_name => @site[:author_name],
+      :author_name => @site.config[:author_name],
       :tags => nil
     }
 
-    identifier = "/posts/" + [ Time.parse(meta[:created_at]).strftime('%Y-%m-%d'), meta[:title] ].map { |a| a.to_url }.join('-') 
-    puts "creating post: " + identifier
-    puts "with meta data: " + meta.inspect
-    content = <<EOF
+    identifier = "/posts/" + [ Time.parse(meta[:created_at]).strftime('%Y-%m-%d'), meta[:title] ].map { |a| a.to_url }.join('-') + "/"
+    exists = @site.items.find { |i| i.identifier == identifier }
+
+    editor_cmd = 'emacsclient --server-file=blog {}'
+    file_extension = '.md'
+
+    if (!exists)
+      puts "creating post: " + identifier
+      content = <<EOF
 tl;dr
 
 <!-- more -->
@@ -33,18 +38,17 @@ tl;dr
 rest of stuff
 EOF
 
-    editor_cmd = 'emacsclient --server-file=blog {}'
-    file_extension = '.md'
     # Create the post
-    data_source = self.site.data_sources[0]
-    data_source.create_item( 
-                            content,
-                            meta,
-                            identifier,
-                            { :extension => file_extension }
-                            )
+      data_source = self.site.data_sources[0]
+      data_source.create_item( 
+                              content,
+                              meta,
+                              identifier,
+                              { :extension => file_extension }
+                              )
+    end
     puts "Opening in editor: content"+identifier+file_extension
-    system(editor_cmd.gsub(/{}/, "content"+identifier+file_extension))
+    system(editor_cmd.gsub(/{}/, "content"+identifier[0..-2]+file_extension))
   end
 end
 
