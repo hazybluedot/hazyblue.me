c1526ca6508dad3c1e232606959991b4e6a73e48
Darren Maczka <dmaczka@vt.edu>
2014-02-15 14:27:22 -0500

    rubified function names
    it is't Ruby-ish to prefix function names with 'get_'

--- lib/helper.rb
+++ lib/helper.rb
@@ -7,15 +7,15 @@ require 'uri'
 require 'summarize'
 
 module PostHelper
-  def get_pretty_date(post)
+  def pretty_date(post)
     attribute_to_time(post[:created_at]).strftime('%B %-d, %Y')
   end
 
-  def get_pub_datetime(post)
+  def pub_datetime(post)
     DateTime.parse(attribute_to_time(post[:created_at]).strftime('%B %-d, %Y %H:%M %z')).rfc3339
   end
 
-  def get_post_raw_start(post)
+  def post_raw_start(post)
     raw_content = post.raw_content
     if raw_content =~ /\s<!-- more -->\s/
       raw_content.partition('<!-- more -->').first
@@ -24,7 +24,7 @@ module PostHelper
     end
   end
 
-  def get_post_topics(post)
+  def post_topics(post)
     encoding_options = {
       :invalid           => :replace,  # Replace invalid byte sequences
       :undef             => :replace,  # Replace anything not defined in ASCII
@@ -34,18 +34,18 @@ module PostHelper
     post.raw_content.encode(Encoding.find('ASCII'), encoding_options).summarize(:topics => true)
   end
 
-  def get_post_summary(post)
+  def post_summary(post)
     encoding_options = {
       :invalid           => :replace,  # Replace invalid byte sequences
       :undef             => :replace,  # Replace anything not defined in ASCII
       :replace           => '',        # Use a blank for those replacements
       :universal_newline => true       # Always break lines with \n
     }
-    raw_content = get_post_raw_start(post)
+    raw_content = post_raw_start(post)
     Kramdown::Document.new(raw_content).to_html.encode(Encoding.find('ASCII'), encoding_options).summarize(:ratio => 25)
   end
 
-  def get_post_start(post)
+  def post_start(post)
     content = post.compiled_content
     if content =~ /\s<!-- more -->\s/
       content = content.partition('<!-- more -->').first +
@@ -71,29 +71,6 @@ module PostHelper
   def tweet_this_url(post)
     post_url = URI::encode(url_for(post))
     "https://twitter.com/intent/tweet?original_referer=#{post_url}&text=#{post[:title]}&url=#{post_url}&via=hazybluedot"
-  end
-end
-
-
-class YouTubeFilter < Nanoc::Filter
-  identifier :youtube
-  type :text
-  
-  def run(content, params={})
-#    content.gsub(/[a-z]+/, 'nanoc rules')
-    content.each_line.map do |line|
-      url = line.gsub('\_','_')
-      youtube_id = nil
-      if url[/^\s*(?:https?:\/\/)?(?:www\.)?(?:youtube\.com(?:\/(?:watch(?:\?(?:\w+=\w+&)+|\?)v=?))|youtu\.be\/)(.+)/]
-        youtube_id = $1
-      end
-      
-      if youtube_id
-        line  = %Q{<p><iframe title="YouTube video player" width="584" height="329" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe></p>}
-      else
-        line
-      end
-    end.join("")
   end
 end
 
